10d2640a24e8db7638cc4ebd630517883331ace9
ljcucc <ljcucc@gmail.com>
2025-09-16 10:44:52 +0800

    refactor: Remove shared_preferences from DirectoryProvider
    This commit refactors the `DirectoryProvider` by removing the `shared_preferences` dependency.
    The directory path will no longer be persisted across app launches.
    
    Also, adds a newline at the end of `macos/Runner/AppDelegate.swift` for consistency.

--- lib/directory_provider.dart
+++ lib/directory_provider.dart
@@ -1,31 +1,16 @@
 import 'package:flutter/foundation.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:file_picker/file_picker.dart';
 
 class DirectoryProvider with ChangeNotifier {
-  static const String _directoryPathKey = 'last_directory_path';
-
   String? _directoryPath;
 
   String? get directoryPath => _directoryPath;
-
-  DirectoryProvider() {
-    _loadDirectoryPath();
-  }
-
-  Future<void> _loadDirectoryPath() async {
-    final prefs = await SharedPreferences.getInstance();
-    _directoryPath = prefs.getString(_directoryPathKey);
-    notifyListeners();
-  }
 
   Future<void> selectDirectory() async {
     String? selectedDirectory = await FilePicker.platform.getDirectoryPath();
 
     if (selectedDirectory != null) {
       _directoryPath = selectedDirectory;
-      final prefs = await SharedPreferences.getInstance();
-      await prefs.setString(_directoryPathKey, selectedDirectory);
       notifyListeners();
     }
   }
